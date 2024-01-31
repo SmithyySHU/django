776800513af95da4bdbc063950a53b725b2a830a
SmithyySHU <c0033267@hallam.shu.ac.uk>
2024-01-31 07:56:00 +0000

    base.html and css done

--- mrreporting/views.py
+++ mrreporting/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request) :
-    return render(request, 'mrreporting/home.html')
+    return render(request, 'mrreporting/home.html', {'title' : 'Welcome'})
 
 def about(request) :
-    return render(request, 'mrreporting/about.html')
+    return render(request, 'mrreporting/about.html', {'title' : 'About Us'})
 
 
 def contact(request) :
-    return render(request, 'mrreporting/contact.html')
+    return render(request, 'mrreporting/contact.html', {'title' : 'Contact Us'})
 
 
 def modules(request) :
-    return render(request, 'mrreporting/modules.html')
+    return render(request, 'mrreporting/modules.html', {'title' : 'Modules'})
